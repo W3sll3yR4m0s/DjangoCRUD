7f762d129c00ace351069670f27106f0fbb7e031
WeslleyRamos <w3sll3yr4m0s@gmail.com>
2022-01-16 21:11:08 -0300

    Criacao dos comportamentos para DELETE. Criacao da pasta static e do arquivo javascript.js. Foi criada a rota para delete em urls.py. Foi criada a funcao delete em views.py. No arquivo javascript.js ha um codigo que verifica se o usuario esta certo de que deseja deletar um determinado dado.

---
+++ app/static/javascript.js
@@ -0,0 +1,19 @@
+(function(win,doc){
+    'use strict';
+
+    // Verifica se o usuário está certo de que deseja deletar o dado.
+    if(doc.querySelector('.btnDel')){
+        let btnDel = doc.querySelectorAll('.btnDel');
+        for(i=0; i < btnDel.length; i++){
+            btnDel[i].addEventListener('click', function(event){
+                if(confirm('Deseja mesmo apagar este dado?')){
+                    return true;
+                }else{
+                    event.preventDefault();
+                }
+            });
+            
+        }
+
+    }
+})(window,document);
