4b86a12349ff74ebbab89bca25b8d2ea54903618
W3sll3yR4m0s <w3sll3yr4m0s@gmail.com>
2022-01-19 14:08:28 -0300

    Primeiro upload deste projeto no Heroku.

--- crud/settings.py
+++ crud/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,7 +131,6 @@ STATICFILES_DIRS = [
 ]
 '''
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Django - Templates
@@ -141,3 +141,5 @@ TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'static', 'templates'),)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
